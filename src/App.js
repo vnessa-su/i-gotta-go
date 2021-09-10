a12c0030e75fa2d11669032a5c1d5bc59b02540c
Vanessa Su <vnessa.su@gmail.com>
2021-09-10 09:58:29 -0400

    :hammer: Add basic content, links and routes to app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { Route, Link } from "react-router-dom";
+import Homepage from "./components/Homepage";
+import LocationForm from "./components/LocationForm";
+import RestroomList from "./components/RestroomList";
+import RestroomDetails from "./components/RestroomDetails";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Link to="/">
+                    <h1>I Gotta Go</h1>
+                </Link>
+            </header>
+            <main>
+                <LocationForm />
+                <Route path="/" exact component={<Homepage />} />
+                <Route
+                    path="/restrooms"
+                    exact
+                    render={() => <RestroomList />}
+                />
+                <Route
+                    path="/restrooms/:id"
+                    exact
+                    render={() => <RestroomDetails />}
+                />
+            </main>
+        </div>
+    );
 }
 
 export default App;

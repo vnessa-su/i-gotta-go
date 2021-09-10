d90f903e0e9c45045d66fbea849dbae868bf9dc6
Vanessa Su <vnessa.su@gmail.com>
2021-09-10 14:24:25 -0400

    :hammer: Add restroom list item component

---
+++ src/components/RestroomItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+function RestroomItem({ data, onClick }) {
+    const history = useHistory();
+
+    const onRestroomClick = () => {
+        console.log("restroom clicked");
+        onClick(data);
+        history.push({ pathname: `/restrooms/${data.id}` });
+    };
+    return (
+        <div>
+            <p onClick={onRestroomClick}>
+                {data.name} - {data.distance} miles
+            </p>
+        </div>
+    );
+}
+
+export default RestroomItem;

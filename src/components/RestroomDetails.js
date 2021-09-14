bb58a8575ddefd0841711b9e44bd58d332d88e4a
Vanessa Su <vnessa.su@gmail.com>
2021-09-14 09:41:57 -0400

    :hammer: Add reroute if details page is no longer valid

--- src/components/RestroomDetails.js
+++ src/components/RestroomDetails.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import DirectionMap from "./DirectionMap";
 
 function RestroomDetails({ data, startLocation }) {
+    const history = useHistory();
+    if (!data.distance) {
+        history.push({ pathname: "/restrooms" });
+        history.go(0);
+    }
+
     const endLatLng = { lat: data.latitude, lng: data.longitude };
     const startLatLng = {
         lat: startLocation.latitude,

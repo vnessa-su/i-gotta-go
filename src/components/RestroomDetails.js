e37a9ae933d4890857e2518d1c045606673af5a8
Vanessa Su <vnessa.su@gmail.com>
2021-09-15 01:01:55 -0400

    :lipstick: Add boostrap styling and icons

--- src/components/RestroomDetails.js
+++ src/components/RestroomDetails.js
@@ -1,6 +1,15 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 import DirectionMap from "./DirectionMap";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faTransgenderAlt,
+    faWheelchair,
+    faBaby,
+} from "@fortawesome/free-solid-svg-icons";
 
 function RestroomDetails({ data, startLocation }) {
     const history = useHistory();
@@ -14,27 +23,91 @@ function RestroomDetails({ data, startLocation }) {
         location: { lat: data.latitude, lng: data.longitude },
     };
 
+    const containerStyle = {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: "10px 0px 10px 0px",
+        backgroundColor: "rgba(255, 255, 255, 0.8)",
+        borderRadius: "10px",
+    };
+
+    const labelStyle = {
+        textAlign: "right",
+        width: "20vw",
+    };
+
+    const infoStyle = {
+        textAlign: "left",
+        width: "70vw",
+    };
+
     return (
         <div>
-            <h2>
-                {data.name} - {data.distance.toFixed(2)} miles
-            </h2>
+            <h1>
+                <span className="location-name-text">{data.name}</span> -{" "}
+                {data.distance.toFixed(2)} miles{" "}
+                {data.unisex && (
+                    <FontAwesomeIcon
+                        icon={faTransgenderAlt}
+                        title="Unisex"
+                        className="filter-icon"
+                    />
+                )}
+                {data.accessible && (
+                    <FontAwesomeIcon
+                        icon={faWheelchair}
+                        title="Accessible"
+                        className="filter-icon"
+                    />
+                )}
+                {data.changing_table && (
+                    <FontAwesomeIcon
+                        icon={faBaby}
+                        title="Changing Table"
+                        className="filter-icon"
+                    />
+                )}
+            </h1>
             <DirectionMap
                 startLocation={startLocation}
                 endLocation={endLocation}
             />
-            <div>
-                <p>
-                    <strong>Address:</strong> {data.street}, {data.city},{" "}
-                    {data.state}
-                </p>
-                <p>
-                    <strong>Directions:</strong> {data.directions}
-                </p>
-                <p>
-                    <strong>Comments:</strong> {data.comment}
-                </p>
-            </div>
+            <Container style={containerStyle}>
+                <Row>
+                    <Col md={5} style={labelStyle}>
+                        <p>
+                            <strong>Address:</strong>
+                        </p>
+                    </Col>
+                    <Col md={7} style={infoStyle}>
+                        <p>
+                            {data.street}, {data.city}, {data.state}
+                        </p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm={5} style={labelStyle}>
+                        <p>
+                            <strong>Location Directions:</strong>
+                        </p>
+                    </Col>
+                    <Col sm={7} style={infoStyle}>
+                        <p>{data.directions}</p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm={5} style={labelStyle}>
+                        <p>
+                            <strong>Comments:</strong>
+                        </p>
+                    </Col>
+                    <Col sm={7} style={infoStyle}>
+                        <p>{data.comment}</p>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
